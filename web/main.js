535f110ef4a55ecc2a090f3d811946a60d721fd5
Sergio-P <elxsergio@gmail.com>
2016-08-17 12:36:39 -0300

    Web first version

--- web/main.js
+++ web/main.js
@@ -4,5 +4,10 @@ let app = angular.module("Educacion",[]);
 
 app.controller("MainController",function($scope){
     var self = $scope;
+    self.page = 1;
+
+    self.setPage = (i) => {
+    	self.page = i;
+    };
 
 });
